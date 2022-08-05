32f5eef911c87ee7f9fb1551670b0bb994da158e
GitHub Action <action@github.com>
2022-08-05 08:09:54 +0000

    Template cleanup

--- src/main/kotlin/com/github/czzcz/ideaplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/czzcz/ideaplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.czzcz.ideaplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.czzcz.ideaplugin.MyBundle
 
 class MyApplicationService {
 
